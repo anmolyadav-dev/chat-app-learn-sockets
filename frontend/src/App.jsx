f6abc1f96f20049629c90f9761b5d04314468c5d
Anmol yadav <anmolyadav03unnao@gmail.com>
2024-12-05 01:26:16 +0530

    basic signup frontend + backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router";
 import Login from "./pages/login/Login.jsx";
 import SignUp from "./pages/signUp/SignUp.jsx";
 import Home from "./pages/home/Home.jsx";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <div className="p-4 h-screen items-center justify-center flex">
-      <Home />
+      <Routes>
+        {/*  // we are using older way of react router dom..--> using Routes and Route */}
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Toaster />
     </div>
   );
 }
