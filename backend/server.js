08525194661d737ee0e27cd27117a99dfe9cb1f6
Anmol yadav <anmolyadav03unnao@gmail.com>
2024-12-06 19:39:01 +0530

    production ready

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
@@ -7,6 +8,9 @@ import userRoutes from "./routes/user.route.js";
 import connect from "./dbConfig/mongoDBconnect.js";
 import { app, server } from "./socket/socket.js";
 // --------------------------------- code ---------------------------------------------
+
+const __dirname = path.resolve();
+
 dotenv.config();
 connect();
 const PORT = process.env.PORT || 5000;
@@ -17,7 +21,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
 app.use("/api/users", userRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(PORT, () => {
   console.log(`Server started at port : ${PORT}`);
 });
