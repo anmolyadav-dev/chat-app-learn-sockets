67911d81407f5b52dee6c54fb99a970bc9d86817
Anmol Yadav <anmolyadav03unnao@gmail.com>
2024-12-08 21:38:51 +0530

    changed socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -13,7 +13,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://react-chat-app-1-54y0.onrender.com/login", {
+      const socket = io("https://react-chat-app-1-54y0.onrender.com/", {
         query: {
           userId: authUser._id,
         },
